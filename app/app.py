a8c906dbddb66194125132a676eb047c88df8107
grelau <gregoire.laustriat@hotmail.fr>
2026-01-29 10:48:21 -0500

    add total_volumes + CSS tweaks

--- app/app.py
+++ app/app.py
@@ -19,7 +19,6 @@ def activities():
     response = table.scan()
 
     events = []
-    print('coucou acti')
 
     for item in response["Items"]:
         events.append({
@@ -61,6 +60,46 @@ def hr_zones():
 
     return jsonify(zones)
 
+def seconds_to_hours_minutes(seconds: int) -> str:
+    if not seconds:
+        return "0 h 0 min"
+
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+
+    return f"{hours} h {minutes} min"
+
+
+@app.route("/api/total-volume")
+def total_volume():
+    start = request.args.get("start")
+    end = request.args.get("end")
+
+    print(start, end)
+
+    response = table.scan(
+        FilterExpression=Attr("startTimeLocal").between(start, end)
+    )
+
+    volume = {
+        "total_activities": 0,
+        "total_distance": 0,
+        "total_duration": 0,
+        "total_elevation_gain": 0,
+    }
+
+    for item in response["Items"]:
+        volume["total_activities"] += 1
+        volume["total_distance"] += int(item.get("distance", 0))
+        volume["total_duration"] += int(item.get("duration", 0))
+        volume["total_elevation_gain"] += int(item.get("elevationGain") or 0)
+
+    volume['total_distance'] = round(volume['total_distance'] / 1000, 2)
+    volume['total_duration'] = seconds_to_hours_minutes(volume['total_duration'])
+    
+    print(volume)
+
+    return jsonify(volume)
 
 if __name__ == "__main__":
     app.run(debug=True)
